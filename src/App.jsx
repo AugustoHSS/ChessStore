87544bddfa58af280a6526fd07542db243b02f9e
Leonardo de Souza Nunes <contato.leonunez@gmail.com>
2022-02-09 14:57:14 -0300

    fix: add code pattern config

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import React from 'react';
-import SignUp from './pages/sign-up';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react'
+import SignUp from './pages/sign-up'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="sign" element={<Lobby className="seila" />} />
         <Route path="/sign-up" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
