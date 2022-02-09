2630d6c99353329a5b4d8bc6a81b918489bad577
Augusto <augustohansel@hotmail.com>
2022-02-08 21:56:20 -0300

    project base

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import SignUp from './pages/sign-up';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
