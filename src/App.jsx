f6509fd44fbfd0f98e387f13b1c9bf8f2babbeda
Augusto <augustohansel@hotmail.com>
2022-02-09 15:22:01 -0300

    feat: add reset css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import React from 'react'
 import SignUp from './pages/sign-up'
+import Reset from './globalStyles/Reset'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="sign" element={<Lobby className="seila" />} />
         <Route path="/sign-up" element={<SignUp />} />
       </Routes>
+      <Reset />
     </BrowserRouter>
   )
 }
